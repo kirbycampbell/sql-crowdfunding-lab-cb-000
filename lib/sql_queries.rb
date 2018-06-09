b8f827a584d024707ddea013edc259db5d82123a
Kirby Campbell <jkirbycampbell@gmail.com>
2018-06-09 20:40:35 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ GROUP BY Users.name ORDER BY USERS.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT Projects.title, SUM(Pledges.amount - Project.funding_goal)
+"SELECT Projects.title, SUM(Pledges.amount - Projects.funding_goal)
 FROM pledges
 LEFT JOIN projects
 ON Pledges.project_id = Projects.id
