febf8650dacb5505d2b51bf6c3cf540d05cbc510
Kirby Campbell <jkirbycampbell@gmail.com>
2018-06-09 21:24:01 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -39,7 +39,11 @@ GROUP BY Users.name ORDER BY user_total_pledge, Users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT Projects.category, SUM(Pledges.amount) as category_pledge_total
+FROM projects
+LEFT JOIN pledges
+ON Projects.id = Pledges.project_id
+GROUP BY Projects.category HAVING Projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
