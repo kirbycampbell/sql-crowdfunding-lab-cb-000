559915a1b84ff480a25df8edbd7d8dc8edfc1918
Kirby Campbell <jkirbycampbell@gmail.com>
2018-06-09 20:29:28 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT Users.name, Users.age, SUM(Pledges.amount)
+"SELECT Users.name, Users.age, Pledges.amount
 FROM Pledges
 LEFT JOIN users
 ON Users.id = Pledges.user_id
