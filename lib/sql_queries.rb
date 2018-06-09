a9183cba18183a2f82feed49c11f23f8698436d2
Kirby Campbell <jkirbycampbell@gmail.com>
2018-06-09 20:33:36 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 FROM Pledges
 LEFT JOIN users
 ON Users.id = Pledges.user_id
-ORDER BY USERS.name;"
+GROUP BY Pledges.amount ORDER BY USERS.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
