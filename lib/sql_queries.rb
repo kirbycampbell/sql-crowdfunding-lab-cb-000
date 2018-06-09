7ba9d443e43a8e84ecbf2eb4346cd1d5db4e2506
Kirby Campbell <jkirbycampbell@gmail.com>
2018-06-09 20:31:00 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,9 +16,9 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT Users.name, Users.age, SUM(Pledges.amount)
-FROM Pledges
-FULL JOIN users
-ON Users.id = Pledges.user_id
+FROM users
+LEFT JOIN pledges
+ON Pledges.user_id = Users.id
 ORDER BY USERS.name;"
 end
 
