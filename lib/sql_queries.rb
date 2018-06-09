4630c3841d3cf146acc6a62452ea35aeef1e0efb
Kirby Campbell <jkirbycampbell@gmail.com>
2018-06-09 20:32:18 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,11 +15,11 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT Users.name, Users.age, SUM(Pledges.amount)
+"SELECT Users.name, Users.age, Pledges.amount
 FROM users
 INNER JOIN pledges
 ON Pledges.user_id = Users.id
-ORDER BY USERS.name;"
+GROUP BY SUM(Pledges.amount) ORDER BY USERS.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
